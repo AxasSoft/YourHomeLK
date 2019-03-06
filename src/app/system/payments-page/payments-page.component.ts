612b75f67e869862578f77a8202c4d264838e727
Fil_work <f.nikiforov@gmail.com>
2019-03-06 14:19:08 +0300

    fashion

--- src/app/system/payments-page/payments-page.component.ts
+++ src/app/system/payments-page/payments-page.component.ts
@@ -9,54 +9,7 @@ import { HttpClient } from '@angular/common/http';
   templateUrl: './payments-page.component.html',
   styleUrls: ['./payments-page.component.scss']
 })
-/*
 
-export class PaymentsPageComponent implements OnInit {
-  payments: any =[{}] ;
-  response:any;
-  array:any;
-  Objvalue =Object.values;
-  headElements = ['pid', 'uid', 'value', 'date', 'type'];
-  searchText: string = '';
-  previous: string;
-  res:any=[{}];
-  test :any;
-constructor (private http: HttpClient,private tableService: MdbTableService){
-  this.http.get('http://tvoydom24.com/api/get_payments.php?ccid=1')
-  .subscribe((response)=>{
-    this.response =response;
-    
-    //this.payments=this.response.payments;
-    //this.payments=[{pid: "1", uid: "55", value: "1024", date: "2018-12-10 00:00:00", type: "hot_water"}]
-    this.test=Object.values(this.response.payments)
-  })
-
-}
-
-ngOnInit(){
- 
-}
-
-
-@HostListener('input') oninput() {
-  
- // this.payments=[{pid: "1", uid: "55", value: "1024", date: "2018-12-10 00:00:00", type: "hot_water"}]
-  //this.payments.push({pid: "2", uid: "55", value: "1024", date: "2018-12-10 00:00:00", type: "hot_water"});
-
-
-//console.log(this.res);
-//console.log(this.searchItems(Object.values(this.response.payments),this.searchText,this.res));
-//console.log(this.searchItems(this.test,this.searchItems,this.res));
-}
-
- 
-
-
-
-}
-
-*/
-  
 export class PaymentsPageComponent implements OnInit {
 
   elements: any  ;
