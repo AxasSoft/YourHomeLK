242db07411ebf6c62b3aab3d8d4dae8ee2e2bd50
Fil_work <f.nikiforov@gmail.com>
2019-03-15 17:13:43 +0300

    stat

--- src/app/system/statistics-page/statistics-page.component.ts
+++ src/app/system/statistics-page/statistics-page.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'yh-statistics-page',
   templateUrl: './statistics-page.component.html',
@@ -7,4 +7,81 @@ import { Component } from '@angular/core';
 })
 
 export class StatisticsPageComponent  {
+  url:any;
+  response:any;
+  
+  
+  
+  public chartType: string = 'bar';
+
+  public chartDatasets:any; 
+
+  public chartLabels:any;
+  
+
+  public chartColors: Array<any> = [
+    {
+      backgroundColor: [
+        'rgba(75, 192, 192, 0.2)',
+        'rgba(75, 192, 192, 0.2)',
+        'rgba(75, 192, 192, 0.2)',
+        'rgba(75, 192, 192, 0.2)',
+        'rgba(75, 192, 192, 0.2)',
+        'rgba(75, 192, 192, 0.2)',
+        'rgba(75, 192, 192, 0.2)',
+        'rgba(75, 192, 192, 0.2)',
+        'rgba(75, 192, 192, 0.2)',
+        'rgba(75, 192, 192, 0.2)',
+        'rgba(75, 192, 192, 0.2)',
+        'rgba(75, 192, 192, 0.2)'
+      ],
+      borderColor: [
+        'rgba(75, 192, 192, 0.2)',
+        'rgba(75, 192, 192, 0.2)',
+        'rgba(75, 192, 192, 0.2)',
+        'rgba(75, 192, 192, 0.2)',
+        'rgba(75, 192, 192, 0.2)',
+        'rgba(75, 192, 192, 0.2)',
+        'rgba(75, 192, 192, 0.2)',
+        'rgba(75, 192, 192, 0.2)',
+        'rgba(75, 192, 192, 0.2)',
+        'rgba(75, 192, 192, 0.2)',
+        'rgba(75, 192, 192, 0.2)',
+        'rgba(75, 192, 192, 0.2)'
+      ],
+      borderWidth: 3,
+    }
+  ];
+
+  public chartOptions: any = {
+    responsive: true
+  };
+  public chartClicked(e: any): void { }
+  public chartHovered(e: any): void { }
+
+
+  constructor(private http: HttpClient){
+    
+    this.url="http://www.tvoydom24.com/api/get_statistics_data.php?ccid=1";
+     this.http.get(this.url).subscribe((response)=>{
+      this.response=response;
+     
+      this.chartDatasets  = [
+        {data:this.response.payments_data, label: 'Общая сумма платежей' }
+      ];
+  this.chartLabels=this.response.rusMonth;
+    
+    })
   }
+  
+
+
+
+
+
+
+
+
+
+
+}
