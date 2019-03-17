1e92754602067f4279807d7d30cbc98ed76f9ab0
vladimirmozgovoy <vladimirwebdeveloper@gmail.com>
2019-03-17 21:52:51 +0300

    orders

--- src/app/system/orders-page/orders-page.component.ts
+++ src/app/system/orders-page/orders-page.component.ts
@@ -1,5 +1,10 @@
-import { Component } from '@angular/core';
 
+import { Component,OnInit, HostListener} from '@angular/core';
+import { MdbTableService } from 'angular-bootstrap-md';
+import { HttpClient } from '@angular/common/http';
+import {MatDatepickerInputEvent} from '@angular/material/datepicker';
+import {Router} from "@angular/router";
+import { CookieService } from 'ngx-cookie-service';
 
 @Component({
   selector: 'yh-orders-page',
@@ -10,4 +15,152 @@ import { Component } from '@angular/core';
 
 export class OrdersPageComponent {
   
+
+
+  dropdownList = [];
+  selectedItems = [];
+  dropdownSettings = {};
+  filterLocalDataBy(searchKey: any,element :any,) {
+
+    if(searchKey===""){
+      return element
+    }
+    return element.filter((obj: Array<any>) => {
+      return Object.keys(obj).some((key: any) => {
+        return (obj[key].toString().toLowerCase()).includes(searchKey);
+      });
+    });
+  }
+ panelOpenState = false;
+  url:any;
+  elements: any  ;
+  test: any;
+  headElements = ['№обр', 'Дата', 'Услуга', 'Адрес','Тема обращения','Статус'];
+  response:any ;
+  post:any;
+payments:any;
+  searchText: string = '';
+  previous: string;
+  fromDate:any;
+  toDate:any;
+  monthFrom: any;
+  monthTo: any;
+  token:any;
+  ccid:any;
+  getFrom(from:MatDatepickerInputEvent<Date>) {
+    this.monthFrom = Number(from.value.getMonth())+1;
+    this.fromDate = from.value.getFullYear()+'-'+this.monthFrom+'-'+from.value.getDate();
+    
+  
+  }
+  getTo(to:MatDatepickerInputEvent<Date>) {
+    this.monthTo=Number(to.value.getMonth())+1;
+    this.toDate=to.value.getFullYear()+'-'+this.monthTo+'-'+to.value.getDate();
+  console.log(this.toDate);
+  if(this.fromDate){
+    this.url="http://www.tvoydom24.com/api/get_payments.php";
+    const body = {from:this.fromDate,to:this.toDate};
+     this.http.post(this.url,body).subscribe((response)=>{
+      this.response=response;
+      this.elements=this.response.payments;
+   console.log(this.response);
+   console.log(body);
+    
+    })
+  }
+  
+  }
+
+  constructor(private http: HttpClient,private tableService: 
+    MdbTableService,private router:Router,private cookieService: CookieService) {
+    //
+    this.token =this.cookieService.get('token');
+    
+    this.ccid =this.cookieService.get('ccid');
+    
+
+this.url="http://www.tvoydom24.com/api/get_orders.php?ccid=3";
+    const body = {token:this.token,ccid:this.ccid};
+     this.http.get(this.url).subscribe((response)=>{
+      this.response=response;
+      this.elements=this.response.orders;
+      
+     
+      
+   
+    
+    })
+
+    
+    
+    
+    
+    
+  
+   }
+
+  @HostListener('input') oninput() {
+    if(this.searchText !=""){
+      this.url="http://www.tvoydom24.com/api/get_orders.php";
+   
+    const body = {search:this.searchText,token:this.token,ccid:3};
+   
+    this.http.post(this.url,body).subscribe((response)=>{
+      this.response=response;
+      this.elements=this.response.orders;
+      console.log(this.elements);
+      console.log(body);
+      
+     
+    })
+    }
+    
+    
+    
+  }
+
+  ngOnInit() {
+    this.dropdownList = [
+      { item_id: 'working', item_text: 'В работе' },
+      { item_id: 'done', item_text: 'Выполнено' },
+      { item_id: 'new', item_text: 'Новый' },
+   
+    ];
+    
+    this.dropdownSettings = {
+      singleSelection: true,
+      idField: 'item_id',
+      textField: 'item_text',
+      
+      
+     
+    };
+  }
+  onItemSelect(item: any) {
+    this.url="http://www.tvoydom24.com/api/get_orders.php";
+    const body = {type:item.item_id,token:this.token,ccid:this.ccid};
+     this.http.post(this.url,body).subscribe((response)=>{
+      this.response=response;
+      this.elements=this.response.orders;
+   console.log(this.response.orders);
+   console.log(body);
+    
+    })
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
--- src/app/system/statistics-page/statistics-page.component.ts
+++ src/app/system/statistics-page/statistics-page.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { TestBed } from '@angular/core/testing';
 @Component({
   selector: 'yh-statistics-page',
   templateUrl: './statistics-page.component.html',
@@ -7,19 +8,59 @@ import { HttpClient } from '@angular/common/http';
 })
 
 export class StatisticsPageComponent  {
+ 
+ 
+  public chartType: string = 'line';
+
+  
+
+  public chartLabels: Array<any> = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+
+  public chartColors: Array<any> = [
+    {
+      backgroundColor: 'rgba(105, 0, 132, .2)',
+      borderColor: 'rgba(200, 99, 132, .7)',
+      borderWidth: 2,
+    }
+    
+  ];
+
+  public chartOptions: any = {
+    responsive: true
+  };
+  public chartClicked(e: any): void { }
+  public chartHovered(e: any): void { }
+
+ 
+ 
+ 
+ test:any;
+ 
   url:any;
   response:any;
   
-  
-  
-  public chartType: string = 'bar';
+  public perType: string = 'doughnut';
 
-  public chartDatasets:any; 
-
-  public chartLabels:any;
+  public barType: string = 'bar';
+  public perDatasets: any; 
+  
+  public perLabels: Array<any> = ['кол-во жильцов', 'квартиры'];
+  public barDatasets:any; 
   
 
-  public chartColors: Array<any> = [
+  public barLabels:any;
+  
+  public perColors: Array<any> = [
+    {
+      backgroundColor: ['#F7464A', '#46BFBD', '#FDB45C', '#949FB1', '#4D5360'],
+      hoverBackgroundColor: ['#FF5A5E', '#5AD3D1', '#FFC870', '#A8B3C5', '#616774'],
+      borderWidth: 2,
+    }
+  ];
+ 
+  
+
+  public barColors: Array<any> = [
     {
       backgroundColor: [
         'rgba(75, 192, 192, 0.2)',
@@ -52,32 +93,52 @@ export class StatisticsPageComponent  {
       borderWidth: 3,
     }
   ];
-
-  public chartOptions: any = {
+   
+  public barOptions: any = {
     responsive: true
   };
-  public chartClicked(e: any): void { }
-  public chartHovered(e: any): void { }
+  public barClicked(e: any): void { }
+  public barHovered(e: any): void { }
+  
+  public perOptions: any = {
+    responsive: true
+  };
+  public perClicked(e: any): void { }
+  public perHovered(e: any): void { }
 
-
-  constructor(private http: HttpClient){
-    
+  getResponse(test,http: HttpClient){
     this.url="http://www.tvoydom24.com/api/get_statistics_data.php?ccid=1";
-     this.http.get(this.url).subscribe((response)=>{
-      this.response=response;
      
-      this.chartDatasets  = [
-        {data:this.response.payments_data, label: 'Общая сумма платежей' }
-      ];
-  this.chartLabels=this.response.rusMonth;
+    this.http.get(this.url).subscribe((response)=>{
+      test=response
+      
+     })
+  return test;
+    }
+  constructor(public http: HttpClient){
+  
+  console.log(this.getResponse(this.test,http));
     
-    })
+    /*this.url="http://www.tvoydom24.com/api/get_statistics_data.php?ccid=1";
+     this.http.get(this.url).subscribe((response)=>{
+      this.response=response;})*/
+  
+      
+      /*this.barDatasets  = [{
+        data:this.response.payments_data, label: 'My First dataset'
+      }];
+  this.barLabels=this.response.rusMonth;*/
+  
+    
+   
+
+
   }
   
+  
+  
 
-
-
-
+  
 
 
 
